3e7506ee72ef221a0828c3dad1f0c7d6a462e8be
asxelot <asxelot@gmail.com>
2015-09-10 23:31:40 +0300

    Finish posts and comments backend

--- api.js
+++ api.js
@@ -1,6 +1,45 @@
-var router = require('express').Router(),
-    Post = require('./models/Posts'),
+var router  = require('express').Router(),
+    Post    = require('./models/Posts'),
     Comment = require('./models/Comments');
+
+
+// Post
+
+router
+  .param('post', function(req, res, next, id) {
+    var query = Post.findById(id);
+
+    query.exec(function(err, post) {
+      if (err) return next(err);
+      if (!post) return res.status(404).send('Post not fined!');
+
+      req.post = post;
+      return next();
+    });
+  })
+  .route('/posts/:post')
+    .get(function(req, res) {
+      req.post.populate('comments', function(err, post) {
+        if (err) return next(err);
+
+        res.json(post);
+      });
+    })
+;
+
+router
+  .route('/posts/:post/upvote')
+    .put(function(req, res, next) {
+      req.post.upvote(function(err, post) {
+        if (err) return next(err);
+
+        res.json(post);
+      });
+    })
+;
+
+
+// Posts
 
 router
   .route('/posts')
@@ -18,6 +57,49 @@ router
         if (err) return next(err);
 
         res.json(post);
+      });
+    })
+;
+
+// Comments
+
+router
+  .param('comment', function(req, res, next, id) {
+    var query = Comment.findById(id);
+
+    query.exec(function(err, comment) {
+      if (err) return next(err);
+      if (!comment) return res.status(404).send('Comment not fined!');
+
+      req.comment = comment;
+      return next();
+    });
+  })
+  .route('/posts/:post/comments')
+    .post(function(req, res, next) {
+      var comment = new Comment(req.body);
+      comment.post = req.post;
+
+      comment.save(function(err, comment) {
+        if (err) return next(err);
+
+        req.post.comments.push(comment);
+        req.post.save(function(err, post) {
+          if (err) return next(err);
+
+          res.json(comment);
+        });
+      });
+    })
+;
+
+router
+  .route('/posts/:post/comments/:comment/upvote')
+    .put(function(req, res, next) {
+      req.comment.upvote(function(err, comment) {
+        if (err) return next(err);
+
+        return res.json(comment);
       });
     })
 ;
--- server.js
+++ server.js
@@ -1,20 +1,38 @@
-var express = require('express'),
-    app = express(),
-    mongoose = require('mongoose'),
-    bodyParser = require('body-parser');
+var express        = require('express'),
+    app            = express(),
+    mongoose       = require('mongoose'),
+    methodOverride = require('method-override'),
+    bodyParser     = require('body-parser');
 
 mongoose
   .connect('mongodb://localhost/ng-reddit')
   .connection
-    .on('error', console.error.bind(console, 'connection error: '))
+    .on('error', console.error.bind(console, 'MongoErr: '))
     .once('open', function() {
       console.log('Connected to DB');
     })
 ;
 
-app.use(bodyParser.json());
-app.use('/api', require('./api'));
+app
+  .use(methodOverride('X-HTTP-Method-Override'))
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(bodyParser.json())
+
+  .use(express.static('public'))
+  .use('/api', require('./api'))
+
+  .get('*', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+  })
+
+  .use(function(err, req, res, next) {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Server error!');
+    }
+  })
+;
 
 app.listen(8080, function() {
-  console.log('App listening');
+  console.log('Server started');
 });
